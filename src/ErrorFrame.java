55b8545858836ba7530b2a53bdf11a4da255040b
tqso <interesting0715@gmail.com>
2023-08-01 15:01:03 +0900

    Change ErrorFrame Bounds

--- src/ErrorFrame.java
+++ src/ErrorFrame.java
@@ -6,6 +6,7 @@ public class ErrorFrame extends JFrame {
     ErrorFrame(String title, String message) {
         setTitle(title);
         setSize(300, 150);
+        setBounds(Main.width/3, Main.height/3,400, 250);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         J_Panel.setLayout(new GridBagLayout());
         J_Panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
